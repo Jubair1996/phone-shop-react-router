3b4c12160e9b027bd1425ce0f0eeb37f2035a5ee
Jubair1996 <abujubair1996@gmail.com>
2023-11-03 00:39:49 +0600

    Fevorite phones added

--- src/Pages/Fevorite/AddToFevorite.jsx
+++ src/Pages/Fevorite/AddToFevorite.jsx
@@ -1,35 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import swal from "sweetalert";
-const PhoneDetailsCard = ({ phone }) => {
+const AddToFevorite = ({ phone }) => {
   const { id, image, phone_name, brand_name } = phone || {};
-
-  const handleAddFevorite = () => {
-    const addedFavoritesArray = [];
-
-    const favoriteItems = JSON.parse(localStorage.getItem("favorites"));
-
-    //jokhon kisu nai tokhon e if vitor dhukba
-    if (!favoriteItems) {
-      addedFavoritesArray.push(phone);
-      localStorage.setItem("favorites", JSON.stringify(addedFavoritesArray));
-      swal("Good job!", "Products added successfully!", "success");
-    } else {
-      const isExits = favoriteItems.find((phone) => phone.id === id);
-
-      if (!isExits) {
-        addedFavoritesArray.push(...favoriteItems, phone);
-        localStorage.setItem("favorites", JSON.stringify(addedFavoritesArray));
-        swal("Good job!", "Products added successfully!", "success");
-      } else {
-        swal("Error!", "No duplicate !", "error");
-      }
-    }
-  };
   return (
-    <div className="flex justify-center items-center h-[80vh] rounded-lg bg-gray-300">
+    <div className="flex justify-center items-centerrounded-lg">
       <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-gray-100 bg-clip-border text-gray-700 shadow-md">
-        <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none shrink-0 rounded-xl bg-clip-border">
+        <div className="relative w-2/5 m-0 overflow-hidden bg-white rounded-r-none shrink-0 rounded-xl bg-clip-border">
           <img
             src={image}
             alt="image"
@@ -45,7 +21,6 @@ const PhoneDetailsCard = ({ phone }) => {
           </h4>
           <Link to='/' className="inline-block" href="#">
             <button
-              onClick={handleAddFevorite}
               className="flex items-center gap-2 px-6 bg-blue-300 py-3 font-sans text-xs font-bold text-center text-pink-500 uppercase align-middle transition-all rounded-lg select-none hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
             >
@@ -72,7 +47,7 @@ const PhoneDetailsCard = ({ phone }) => {
     </div>
   );
 };
-PhoneDetailsCard.propTypes = {
+AddToFevorite.propTypes = {
   phone: PropTypes.object.isRequired,
 };
-export default PhoneDetailsCard;
+export default AddToFevorite;
--- src/Pages/Fevorite/Fevorite.jsx
+++ src/Pages/Fevorite/Fevorite.jsx
@@ -1,10 +1,78 @@
+import { useEffect, useState } from "react";
+import AddToFevorite from "./AddToFevorite";
 
 const Fevorite = () => {
-    return (
-        <div>
-            Fevorite
+  const [fevorites, setFevorites] = useState([]);
+  const [noDataFound, setNoDataFound] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  //   const [noDataFound, setNoDataFound] = useState(false)
+
+  useEffect(() => {
+    const favoriteItems = JSON.parse(localStorage.getItem("favorites"));
+
+    if (favoriteItems) {
+      setFevorites(favoriteItems);
+      const total = favoriteItems.reduce(
+        (preValue, currentItem) => preValue + currentItem.price,
+        0
+      );
+
+      //   console.log(total);
+
+      setTotalPrice(total);
+    } else {
+      setNoDataFound("No data Found");
+    }
+  }, []);
+  //   console.log(fevorites);
+  const handleRemove = () => {
+    localStorage.clear();
+    setFevorites([]);
+    setNoDataFound("No data Found");
+  };
+  return (
+    <div>
+      {fevorites.length > 0 && (
+       <div>
+         <button
+          onClick={handleRemove}
+          className="px-5 block mx-auto rounded bg-green-600 my-8 text-white font-semibold text-xl"
+        >
+          Delete Favorite Phone
+        </button>
+        <h1 className="text-center text-xl font-semibold mb-4">Total price : {totalPrice}</h1>
+       </div>
+      )}
+
+      {noDataFound ? (
+        <p className="flex justify-center items-center h-[80vh]">
+          {noDataFound}
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {isShow
+            ? fevorites.map((phone) => (
+                <AddToFevorite key={phone.id} phone={phone}></AddToFevorite>
+              ))
+            : fevorites
+                .slice(0, 2)
+                .map((phone) => (
+                  <AddToFevorite key={phone.id} phone={phone}></AddToFevorite>
+                ))}
         </div>
-    );
+      )}
+
+      {fevorites.length > 2 && (
+        <button
+          onClick={() => setIsShow(!isShow)}
+          className="px-5 block mx-auto rounded bg-green-600 my-8 text-white font-semibold text-xl"
+        >
+          {isShow ? "See Less" : "See More"}
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Fevorite;

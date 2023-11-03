8663bbe21a7c8c2ffde8fe0864cdbd201bcb3c85
Jubair1996 <abujubair1996@gmail.com>
2023-11-03 14:07:27 +0600

    Project done

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 
 const Root = () => {
-    return (
-        <div className="max-w-[1250px] mx-auto">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  const loc = useLocation();
+
+  console.log(loc);
+
+  useEffect(() => {
+    console.log(loc.pathname);
+
+    if (loc.pathname === "/") {
+      document.title = `Phone-home`;
+    } else {
+      document.title = `Phone ${loc.pathname.replace("/", "-")}`;
+    }
+
+    if (loc.state) {
+      document.title = ` ${loc.state}`;
+    }
+  }, [loc.pathname]);
+  return (
+    <div className="max-w-[1250px] mx-auto">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;
